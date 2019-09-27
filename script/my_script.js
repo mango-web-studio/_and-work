9e4416da050f3640a43b0786e355623144b7d58d
Maksym Kravchuk <kravchukmaxym@gmail.com>
2019-09-27 18:18:31 +0300

    Improved my_script file (one selected option is hidden).

--- script/my_script.js
+++ script/my_script.js
@@ -1,23 +1,21 @@
 window.onload = function() {
     // stylization of "select" elements
-    let divWrapSelect, selElmnt, a, b, c;
+    let divWrapSelects, selElmnt, a, b, c;
     /*look for any elements with the class "select_wrap":*/
-    divWrapSelect = document.getElementsByClassName("select_wrap");
-
-    for (let i = 0; i < divWrapSelect.length; i++) {
-        selElmnt = divWrapSelect[i].getElementsByTagName("select")[0];
+    divWrapSelects = document.getElementsByClassName("select_wrap");
+    for (let i = 0; i < divWrapSelects.length; i++) {
+        selElmnt = divWrapSelects[i].getElementsByTagName("select")[0];
         /*for each element, create a new DIV that will act as the selected item:*/
-        a = document.createElement("DIV");
+        a = document.createElement("div");
         a.setAttribute("class", "select-selected");
         a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-        divWrapSelect[i].appendChild(a);
+        divWrapSelects[i].appendChild(a);
         /*for each element, create a new DIV that will contain the option list:*/
-        b = document.createElement("DIV");
+        b = document.createElement("div");
         b.setAttribute("class", "select-items select-hide");
-
         for (let j = 0; j < selElmnt.length; j++) {
             /*for each option in the original select element, create a new DIV that will act as an option item:*/
-            c = document.createElement("DIV");
+            c = document.createElement("div");
             c.innerHTML = selElmnt.options[j].innerHTML;
             c.addEventListener("click", function() {
                 /*when an item is clicked, update the original select box, and the selected item:*/
@@ -25,19 +23,23 @@ window.onload = function() {
                 s = this.parentElement.parentElement.getElementsByTagName("select")[0];
                 // div element with class name "select-selected"
                 h = this.parentElement.previousSibling;
-                // the input element with class name "tarrif_input"
-                tarrifInput = this.parentElement.parentElement.parentElement.nextElementSibling.querySelector('input');
                 for (let i = 0; i < s.length; i++) {
                     if (s.options[i].innerHTML == this.innerHTML) {
                         s.selectedIndex = i;
-                        // write in the input element value of the slected option
-                        tarrifInput.value = s.getElementsByTagName('option')[s.selectedIndex].getAttribute('data-price');
+                        if (this.parentElement.parentElement.parentElement.classList.contains('meeting_room_description_tarrif_plan')) {
+                            // the input element with class name "tarrif_input"
+                            tarrifInput = this.parentElement.parentElement.parentElement.nextElementSibling.querySelector('input');
+                            if (tarrifInput) {
+                                // write in the input element value of the slected option
+                                tarrifInput.value = s[s.selectedIndex].getAttribute('data-price');
+                            }
+                        }
                         h.innerHTML = this.innerHTML;
                         y = this.parentElement.getElementsByClassName("same-as-selected");
                         for (let k = 0; k < y.length; k++) {
                             y[k].removeAttribute("class");
                         }
-                        this.setAttribute("class", "same-as-selected");
+                        this.setAttribute("class", "same-as-selected select-hide");
                         break;
                     }
                 }
@@ -45,7 +47,8 @@ window.onload = function() {
             });
             b.appendChild(c);
         }
-        divWrapSelect[i].appendChild(b);
+        divWrapSelects[i].appendChild(b);
+        
         a.addEventListener("click", function(e) {
             /*when the select box is clicked, close any other select boxes, and open/close the current select box:*/
             e.stopPropagation();
@@ -62,14 +65,14 @@ window.onload = function() {
         y = document.getElementsByClassName("select-selected");
         for (i = 0; i < y.length; i++) {
             if (elmnt == y[i]) {
-            arrNo.push(i)
+                arrNo.push(i)
             } else {
-            y[i].classList.remove("select-arrow-active");
+                y[i].classList.remove("select-arrow-active");
             }
         }
         for (i = 0; i < x.length; i++) {
             if (arrNo.indexOf(i)) {
-            x[i].classList.add("select-hide");
+                x[i].classList.add("select-hide");
             }
         }
     }
